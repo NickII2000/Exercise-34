4d622486b147cd9440fde451a981094c95be1c78
Nick <Nick_II2000@mail.ru>
2022-05-16 19:25:08 +0600

    4-th commit

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 // touchcancel
 
 // touches
+// targetTouches
+// changedTouches
 
 window.addEventListener('DOMContentLoaded', () => {
     const box = document.querySelector('.box');
@@ -13,7 +15,11 @@ window.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
         console.log('touchstart');
         console.log(e.touches);
+        console.log(e.targetTouches);
+        console.log(e.changedTouches);
+        console.log(e.targetTouches[0].pageX);
     });
+
     // box.addEventListener('touchmove', (e) => {
     //     e.preventDefault();
     //     console.log('touchmove');
@@ -23,24 +29,4 @@ window.addEventListener('DOMContentLoaded', () => {
     //     console.log('touchend');
     // });
 
-    // box.addEventListener('touchstart', (e) => {
-    //     e.preventDefault();
-    //     console.log('touchstart');
-    //     console.log(e.touches);
-    //     console.log(e.targetTouches);
-    //     console.log(e.changedTouches);
-    // });
-
-    // box.addEventListener('touchmove', (e) => {
-    //     e.preventDefault();
-    //     console.log(e.targetTouches[0].pageX);
-    // });
-    // box.addEventListener('touchend', (e) => {
-    //     e.preventDefault();
-    //     console.log('touchend');
-    // });
 });
-
-// touches
-// targetTouches
-// changedTouches
