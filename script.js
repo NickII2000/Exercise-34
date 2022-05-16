c45cc9523677dddac503129df613ca7e6844776a
Nick <Nick_II2000@mail.ru>
2022-05-16 19:18:12 +0600

    3-th commit

--- script.js
+++ script.js
@@ -5,15 +5,23 @@
 // touchleave
 // touchcancel
 
+// touches
+
 window.addEventListener('DOMContentLoaded', () => {
     const box = document.querySelector('.box');
     box.addEventListener('touchstart', (e) => {
         e.preventDefault();
         console.log('touchstart');
         console.log(e.touches);
-        console.log(e.targetTouches);
-        console.log(e.changedTouches);
     });
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+    //     console.log('touchmove');
+    // });
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+    //     console.log('touchend');
+    // });
 
     // box.addEventListener('touchstart', (e) => {
     //     e.preventDefault();
